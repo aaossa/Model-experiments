fcb1024b11bbe7ea85e4934f71c21026c3b71c32
Antonio Ossa <aaossa@uc.cl>
2020-06-12 22:41:11 -0400

    :sparkles: Add script for Google Colaboratory
    Custom script to setup VMs faster
    
    Signed-off-by: Antonio Ossa <aaossa@uc.cl>

---
+++ scripts/colaboratory.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+REQUIREMENTS_TXT=${1:-requirements/dev.txt}
+
+
+# Install project requirements
+echo ">> Installing requirements.txt"
+if [ -f "${REQUIREMENTS_TXT}" ]; then
+    pip install -r "$REQUIREMENTS_TXT"
+else
+    echo ERROR: Failed to find "${REQUIREMENTS_TXT}". Please provide an existing path to a requirements file.
+    exit 1
+fi
+echo
+
+# Aditional setup
+echo ">> Additional setup"
+sudo apt-get install python3-gdbm
+echo
+
+# Explore GPUs if available
+echo ">> Explore GPUs if available"
+ln -sf /opt/bin/nvidia-smi /usr/bin/nvidia-smi
+if ! type nvidia-smi >/dev/null 2>&1; then
+    echo Command 'nvidia-smi' not found
+else
+    nvidia-smi
+fi

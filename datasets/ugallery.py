73b5a1f7edc86a0a793a9bc1a5a3986b7af6d22f
Antonio Ossa <aaossa@uc.cl>
2020-06-14 22:35:04 -0400

    :zap: Improve performance of UGallery Dataset
    Improved preprocessing times and also items retrieval performance
    
    Signed-off-by: Antonio Ossa <aaossa@uc.cl>

--- datasets/ugallery.py
+++ datasets/ugallery.py
@@ -3,6 +3,10 @@
 This module contains a that contains the information about the
 UGallery dataset, to be accessible from PyTorch.
 """
+import errno
+import os
+
+import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset
 
@@ -15,79 +19,77 @@ class UGalleryDataset(Dataset):
     """Represents the UGallery Dataset as a PyTorch Dataset.
 
     Attributes:
-        triples: Dataset triples in the form (profile, pi, ni).
-        profile_sizes: Size of each profile, for faster calculation.
-        embedding: Precomputed features for each item.
-        transform: Transforms for each item.
+        profile_sizes: Size of each user profile.
+        unique_profiles: Actual profile data to save space.
+        profile, pi, ni: Dataset triples (in different arrays).
+        transform: Transforms for each sample.
     """
 
-    def __init__(self, csv_file, embedding, transform=None):
+    def __init__(self, csv_file, transform=None, id2index=None):
         """Inits a UGallery Dataset.
 
         Args:
             csv_file: Path (string) to the triplets file.
-            embedding: Numpy array of items features.
             transform: Optional. Torchvision like transforms.
+            id2index: Optional. Transformation to apply on items.
         """
-        # Dataframe
-        self.triples = pd.read_csv(csv_file)
-        self.triples["profile"] = self.triples["profile"].map(
-            lambda p: p[1:-1].replace("'", "").split(", ")
-        )
-        # Caching profile sizes
-        self.profile_sizes = tuple(self.triples["profile"].map(len))
-        self.embedding = embedding
-        self.transform = transform
-        self.__ready = False
-
-    def prepare(self, id2index=None):
-        """Prepare dataset for training/evaluation procedure.
-
-        Map ids into indexes (in the embedding) if necessary and
-        transforms triples from a Pandas DataFrem into a Numpy
-        object for faster processing.
-
-        Args:
-            id2index: Optional. Mapping from item ids to indexes in
-                the features container (self.embedding).
-        """
-        if self.__ready:
-            raise Exception("Dataset was already prepared")
+        # Data sources
+        if not os.path.isfile(csv_file):
+            raise FileNotFoundError(
+                errno.ENOENT, os.strerror(errno.ENOENT), csv_file,
+            )
+        self.__source_file = csv_file
+        # Load triples from dataframe
+        triples = pd.read_csv(self.__source_file)
+        # Process profile elements
         if id2index:
-            self.__apply_mapping(id2index)
-        self.triples = self.triples.to_numpy()
-        self.__ready = True
-        print("Dataset is ready")
-
-    def __apply_mapping(self, id2index):
-        """Applies mapping from item ids to indexes.
-
-        Args:
-            id2index: Mapping from item ids to indexes in the
-                features container (self.embedding).
-        """
-        def map_id2index(element):
-            if type(element) is list:
-                return [id2index[e] for e in element]
-            else:
-                return id2index[element]
-        self.triples = self.triples.applymap(map_id2index)
+            # Note: Assumes id is str and index is int
+            def map_id2index(element):
+                if type(element) is list:
+                    return [id2index[e] for e in element]
+                else:
+                    return id2index[str(element)]
+            triples["profile"] = triples["profile"].map(lambda p: p.split())
+            triples = triples.applymap(map_id2index)
+            triples["profile"] = triples["profiles"].map(lambda p: " ".join(p))
+        self.profile_sizes = np.fromiter(
+            map(lambda p: p.count(" ") + 1, triples["profile"]),
+            dtype=int, count=len(triples),
+        )
+        # Mapping to unique profiles
+        self.unique_profiles = triples["profile"].unique()
+        profile2index = {k: v for v, k in enumerate(self.unique_profiles)}
+        triples["profile"] = triples["profile"].map(lambda p: profile2index[p])
+        self.unique_profiles = self.unique_profiles.astype(np.string_)
+        # Using numpy arrays for faster lookup
+        self.profile = triples["profile"].to_numpy(copy=True)
+        self.pi = triples["pi"].to_numpy(copy=True)
+        self.ni = triples["ni"].to_numpy(copy=True)
+        # Common setup
+        self.transform = transform
 
     def __len__(self):
-        return len(self.triples)
+        return len(self.pi)
 
     def __getitem__(self, idx):
-        profile = self.embedding[self.triples[idx, 0], :]
-        pi = self.embedding[self.triples[idx, 1]]
-        ni = self.embedding[self.triples[idx, 2]]
+        prof = self.profile[idx]
+        if isinstance(idx, int) or isinstance(idx, np.number):
+            profile = np.fromstring(
+                self.unique_profiles[prof], dtype=int, sep=" ",
+            )
+        else:
+            profile = self.unique_profiles[prof]
+            profile = b" ".join(profile)
+            profile = np.fromstring(profile, dtype=int, sep=" ")
+            profile = profile.reshape((len(idx), -1))
 
-        sample = {
-            "profile": profile,
-            "pi": pi,
-            "ni": ni,
-        }
+        return (
+            profile,
+            self.pi[idx],
+            self.ni[idx],
+        )
 
-        if self.transform:
-            sample = self.transform(sample)
+        # if self.transform:
+        #     sample = self.transform(sample)
 
-        return sample
+        # return sample

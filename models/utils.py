7fe316ec2f3593fc8307e50c4f9896b3ee01d76d
Antonio Ossa <aaossa@uc.cl>
2020-06-30 03:30:31 -0400

    :sparkles: Add method to retrieve existing models
    Models must be available via torchvision.models. A list can be seen in
    the docs ([1])
    
    [1]: https://pytorch.org/docs/stable/torchvision/models.html#torchvision-models
    
    Signed-off-by: Antonio Ossa <aaossa@uc.cl>

--- models/utils.py
+++ models/utils.py
@@ -1,4 +1,13 @@
 import torch
+import torchvision.models as models
+
+
+def get_model_by_name(model_name, pretrained=True):
+    model = models.__dict__[model_name](pretrained=pretrained)
+    model = torch.nn.Sequential(*list(model.children()))[:-1]
+    for param in model.parameters():
+        model.requres_grad = False
+    return model
 
 
 def save_checkpoint(checkpoint_path, **components):

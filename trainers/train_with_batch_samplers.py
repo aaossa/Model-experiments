82762ba3f36b9eed1f12b9e9df52927d7462ff19
Antonio Ossa <aaossa@uc.cl>
2020-08-12 18:56:48 -0400

    :zap: Improve memory usage in checkpoint creation
    Instead of copying the model directly in GPU, copy its state to CPU,
    also save models in checkpoints directly in CPU
    
    Signed-off-by: Antonio Ossa <aaossa@uc.cl>

--- trainers/train_with_batch_samplers.py
+++ trainers/train_with_batch_samplers.py
@@ -6,7 +6,7 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 from tqdm.notebook import tqdm
 
-from models.utils import save_checkpoint
+from models.utils import get_cpu_copy, save_checkpoint
 
 
 def train_with_batch_samplers(
@@ -153,7 +153,7 @@ def train_with_batch_samplers(
                     best_validation_acc = epoch_acc
                     best_validation_loss = epoch_loss
                     save_checkpoint(
-                        checkpoint_dst, model=model,
+                        checkpoint_dst, model=get_cpu_copy(model),
                         criterion=criterion, optimizer=optimizer, scheduler=scheduler,
                         epoch=scheduler.last_epoch, accuracy=best_validation_acc,
                     )
@@ -208,20 +208,23 @@ def train_with_batch_samplers(
 
     # Copy last model weights
     print(">> Copy last model")
-    last_model = copy.deepcopy(model)
+    last_model = copy.deepcopy(get_cpu_copy(model))
 
-    # Load best modelweights
+    # Load best model weights
     print(">> Load best model")
-    checkpoint = torch.load(checkpoint_dst)
+    checkpoint = torch.load(checkpoint_dst, map_location=torch.device("cpu"))
     model.load_state_dict(checkpoint["model"])
+
+    # Move model back to device
+    model.to(device)
 
     # Save last state
     print(">> Save last state")
     save_checkpoint(
-        checkpoint_dst, model=model,
+        checkpoint_dst, model=get_cpu_copy(model),
         criterion=criterion, optimizer=optimizer, scheduler=scheduler,
         epoch=scheduler.last_epoch, accuracy=best_validation_acc,
         last_model=last_model, last_accuracy=epoch_acc,
     )
 
-    return model, checkpoint["accuracy"], checkpoint["epoch"], last_model
+    return model, checkpoint["accuracy"], checkpoint["epoch"]
